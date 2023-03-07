f44e466283698fab3164bd8052b285d77f65d3c0
Isaac M Bazian <bazia001@umn.edu>
2023-03-07 09:44:55 -0600

    Working on data import, still need to relabel variables but it's coming along

--- R/week7.R
+++ R/week7.R
@@ -1,3 +1,18 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+
+
+
+# Data Import and Cleaning
+week7_tbl <- read_csv(file = "../data/week3.csv", col_types = "?Tffiiiiiiiiii") %>% 
+  mutate(timeStart = ymd_hms(timeStart)) %>% #This step is necessary because when I tried to do this in the col_types above, timeStart had NAs
+  filter(q6 == 1) %>% 
+  select(-q6) %>% 
+  mutate(timeSpent = difftime(timeEnd, timeStart, units = "mins"))
+
+
+
+
 
